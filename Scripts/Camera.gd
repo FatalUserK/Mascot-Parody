8955483fdbd8946ff7495cb0fb1c89f38077c841
UserK <111083281+FatalUserK@users.noreply.github.com>
2024-04-15 01:34:36 +0100

    did some work
    shouldve documented changes better for examiners to see probs but oh well (i didnt do much tbh and i dont really see much use in github stuff outside of switching devices, occasional backups are nice but thats about it)

---
+++ Scripts/Camera.gd
@@ -0,0 +1,26 @@
+extends Camera2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _unhandled_input(event: InputEvent):
+	if event is InputEventMouseMotion:
+		if event.button_mask == MOUSE_BUTTON_MASK_MIDDLE:
+			position -= event.relative / zoom
+			print("Position is" + str(position))
+	pass
+
+
+var zoom_min = Vector2(1,1)
+var zoom_max = Vector2(3,3)
+var zoom_speed = Vector2(.75,.75)
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+				if zoom > zoom_min:
+					zoom -= zoom_speed
+			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+				if zoom < zoom_max:
+					zoom += zoom_speed
+	pass

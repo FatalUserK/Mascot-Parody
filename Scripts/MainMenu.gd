58f7ee52a5a43c86b02470124ae353125cdc1cc9
UserK <111083281+FatalUserK@users.noreply.github.com>
2024-04-16 17:09:56 +0100

    working on click dragging ui

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -3,17 +3,17 @@ extends Control
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://MainLevel.tscn")
+	get_tree().change_scene_to_file("res://Scenes/MainLevel.tscn")
 	
 	pass # Replace with function body.
 
 
 func _on_continue_pressed():
-	get_tree().quit()
+	get_tree().change_scene_to_file("res://Scenes/LevelOld.tscn")
 
 
 func _on_options_pressed():
-	get_tree().change_scene_to_file("res://OptionsMenu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/OptionsMenu.tscn")
 
 
 func _on_quit_pressed():

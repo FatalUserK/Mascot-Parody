a67a126cb915ce985e18552c36883e14cd3020c3
UserK <111083281+FatalUserK@users.noreply.github.com>
2024-04-26 16:14:00 +0100

    TO CLARIFY: i lost some of my changes and i cant find them :(

--- Scenes/Subscenes/UI Draggable Test.gd
+++ Scenes/Subscenes/UI Draggable Test.gd
@@ -1,21 +1,22 @@
 extends TextureRect
 
+var objectID
+var placementCost
+var previewDataOverride = {}
 
 # Called when the node enters the scene tree for the first time.
-func _get_drag_data(at_position):
+func _get_drag_data(position: Vector2):
 	
-	var preview_texture = TextureRect.new()
+	var data = {}
+	data["ID"] = objectID
+	data["cost"] = placementCost
+	data["previewOverride"] = previewDataOverride
 	
-	preview_texture.texture = texture
-	preview_texture.expand_mode = 1
-	preview_texture.size = Vector2(30,30)
-	
-	var preview = Control.new()
-	preview.add_child(preview_texture)
-	
-	set_drag_preview(preview)
-	
-	return texture
+	var dragPreview = DRAGPREVIEW.instance()
+	dragPreview.texture = texture_normal
+	add_child(dragPreview)
+		
+	return data
 	
 func _can_drop_data(at_position, data):
 	return data is Texture2D

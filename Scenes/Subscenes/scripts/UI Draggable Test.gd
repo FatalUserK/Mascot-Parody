a0db4d2e40caf8e3d8445636435e91c46e439e25
UserK <111083281+FatalUserK@users.noreply.github.com>
2024-05-14 04:12:42 +0100

    I! DID! SOMETHING!!!
    i finally got past my bullshit "programmer's block" i think, i fixed an issue and honestly felt happy about it, probably shouldnt be satisfied- but im not about to throw away this free dopamine incentivising me to work on it more tomorrow, free productivity drug!!

--- Scenes/Subscenes/scripts/UI Draggable Test.gd
+++ Scenes/Subscenes/scripts/UI Draggable Test.gd
@@ -4,16 +4,19 @@ var objectID
 var placementCost
 var previewDataOverride = {}
 
+var DRAGPREVIEW = preload("res://Scenes/Subscenes/DragPreview.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _get_drag_data(position: Vector2):
+	var slot = get_parent().get_name()
 	
 	var data = {}
 	data["ID"] = objectID
 	data["cost"] = placementCost
 	data["previewOverride"] = previewDataOverride
 	
-	var dragPreview = DRAGPREVIEW.instance()
-	dragPreview.texture = texture_normal
+	var dragPreview = DRAGPREVIEW.instantiate()
+
 	add_child(dragPreview)
 		
 	return data
